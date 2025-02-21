fbed867dcecb76cec687b6754c2b53327abf3ad7
undisassemble <181021343+undisassemble@users.noreply.github.com>
2025-02-21 01:45:34 -0500

    Kinda added the old installer again because I couldnt get CPack working

---
+++ Installer/build.bat
@@ -0,0 +1,64 @@
+@ECHO off
+setlocal ENABLEDELAYEDEXPANSION
+REM !!THIS IS RUN AUTOMATICALLY DURING RELEASE BUILDS, DO NOT RUN MANUALLY!!
+
+SET INSTALLSIZE=0
+REM SET PACKBINS=1
+
+REM Set this to 0 to force disable UPX packing
+REM IF !PACKBINS! EQU 1 (
+REM 	WHERE upx
+REM 	IF !ERRORLEVEL! NEQ 0 (
+REM 		SET PACKBINS=0
+REM 		ECHO Can't pack with UPX
+REM 	)
+REM )
+
+REM Delete old installer
+IF EXIST "Installer.exe" (
+	ECHO Deleting outdated installer
+	DEL "Installer.exe"
+)
+
+REM Check if makensis exists
+WHERE makensis
+IF !ERRORLEVEL! EQU 0 (
+	ECHO Preparing to build installer
+	
+	REM Copy required files
+	COPY /B /V "..\..\Installer\yap.bat" "yap.bat"
+	COPY /B /V "..\..\LICENSE" "LICENSE"
+    COPY /B /V "..\..\Installer\main.nsi" "main.nsi"
+
+    REM Calculate installed size
+	FOR /F "usebackq" %%A IN ('asmjit.dll') DO SET /a INSTALLSIZE=INSTALLSIZE+%%~zA
+	FOR /F "usebackq" %%A IN ('glfw3.dll') DO SET /a INSTALLSIZE=INSTALLSIZE+%%~zA
+	FOR /F "usebackq" %%A IN ('imgui.dll') DO SET /a INSTALLSIZE=INSTALLSIZE+%%~zA
+	FOR /F "usebackq" %%A IN ('LICENSE') DO SET /a INSTALLSIZE=INSTALLSIZE+%%~zA
+	FOR /F "usebackq" %%A IN ('yap.bat') DO SET /a INSTALLSIZE=INSTALLSIZE+%%~zA
+	FOR /F "usebackq" %%A IN ('yap.dll') DO SET /a INSTALLSIZE=INSTALLSIZE+%%~zA
+	FOR /F "usebackq" %%A IN ('yap.h') DO SET /a INSTALLSIZE=INSTALLSIZE+%%~zA
+	FOR /F "usebackq" %%A IN ('YAPClient.exe') DO SET /a INSTALLSIZE=INSTALLSIZE+%%~zA
+	FOR /F "usebackq" %%A IN ('zydis.dll') DO SET /a INSTALLSIZE=INSTALLSIZE+%%~zA
+	SET /a INSTALLSIZE=INSTALLSIZE/1000
+
+	REM Pack release files
+	REM IF !PACKBINS! NEQ 0 (
+	REM 	ECHO Compressing release files
+	REM 	upx -9 "asmjit.dll" "glfw3.dll" "yap.dll" "YAPClient.exe" "Zydis.dll"
+	REM )
+	
+	REM Build
+	ECHO Building installer
+	makensis /DINSTALLSIZE=!INSTALLSIZE! /V1 "main.nsi"
+	IF !ERRORLEVEL! EQU 0 (
+		ECHO Finished building installer
+	) ELSE (
+		ECHO Failed to build installer
+	)
+) ELSE (
+    ECHO "Couldn't find makensis, skipping build."
+)
+endlocal
+
+pause
---
+++ Installer/main.nsi
@@ -0,0 +1,54 @@
+; Setup
+!define VERSION "0.0.0"
+Name "Yet Another Packer"
+OutFile "Installer.exe"
+InstallDir "$PROGRAMFILES64\Yet Another Packer"
+RequestExecutionLevel admin
+VIProductVersion "${VERSION}.0"
+VIFileVersion "${VERSION}.0"
+VIAddVersionKey "ProductVersion" "${VERSION}"
+VIAddVersionKey "FileVersion" "${VERSION}"
+VIAddVersionKey "FileDescription" "Protector for x64 native Windows PEs."
+SetCompressor lzma
+
+; Installer
+Section
+    ; bin
+    CreateDirectory "$INSTDIR\bin"
+    SetOutPath "$INSTDIR\bin"
+    File asmjit.dll
+    File glfw3.dll
+    File zydis.dll
+    File imgui.dll
+    File YAPClient.exe
+    File yap.bat
+
+    ; SDK
+    CreateDirectory "$INSTDIR\SDK"
+    SetOutPath "$INSTDIR\SDK"
+    File yap.dll
+    File yap.h
+
+    ; Add to programs list
+	WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\YAP" "DisplayName" "Yet Another Packer"
+	WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\YAP" "DisplayVersion" "${VERSION}"
+	WriteRegStr HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\YAP" "UninstallString" "$\"$INSTDIR\Uninstall.exe$\""
+	WriteRegDWORD HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\YAP" "NoModify" 1
+	WriteRegDWORD HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\YAP" "NoRepair" 1
+	!ifdef INSTALLSIZE
+		WriteRegDWORD HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\YAP" "EstimatedSize" ${INSTALLSIZE}
+	!endif
+
+    ; Other files
+    CreateShortCut "$SMPROGRAMS\Yet Another Packer.lnk" "$INSTDIR\bin\YAPClient.exe"
+    SetOutPath $INSTDIR
+    File LICENSE
+    WriteUninstaller "$INSTDIR\Uninstall.exe"
+SectionEnd
+
+; Uninstaller
+Section "Uninstall"
+    Delete "$SMPROGRAMS\Yet Another Packer.lnk"
+    DeleteRegKey HKLM "Software\Microsoft\Windows\CurrentVersion\Uninstall\YAP"
+    RMDir /r $INSTDIR
+SectionEnd
